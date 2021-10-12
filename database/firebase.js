94afc77ff02736359c674049066e015a3b4dc9ef
WarunaPrabashwara <55828398+WarunaPrabashwara@users.noreply.github.com>
2021-10-12 05:26:11 -0700

    Update firebase.js
    ikj

--- database/firebase.js
+++ database/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXY5LpY2-S1KPOsz4LzSLU7ffxRTSN1Mk",
-    authDomain: "secondeffort-38704.firebaseapp.com",
-    projectId: "secondeffort-38704",
+    apiKey: "AIzaSyDXY5L5465pY2-S1KPOsz4LzSLU7ffxRTSN1Mk",
+    authDomain: "secondef3545fort-38704.firebaseapp.com",
+    projectId: "second545effort-38704",
     storageBucket: "secondeffort-38704.appspot.com",
-    messagingSenderId: "383534170520",
-    appId: "1:383534170520:web:d098ef7d839b129496abed"
+    messagingSenderId: "383534170545520",
+    appId: "1:383534170520:web:d098ef7d839b1251549496abed"
 };
 
 firebase.initializeApp(firebaseConfig);
